6b076f997ab9b71380301d5c45d5a6f02dcac786
n350071 <n350071@gmail.com>
2019-10-27 03:33:54 +0900

    :up: first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Scene downloader
+
+### Before use
+
+please make a `.env` file and set following constants for your environment.
+
+```sh
+PASSWORD=12345678
+ALBUM_URL=valkj82fakx
+```
+
+### Use
+Just type following, and check the download directory later.
+
+```sh
+ruby download.rb
+```
+
---
+++ download.rb
@@ -0,0 +1,37 @@
+require 'mechanize'
+require 'pry'
+require 'pry-byebug'
+
+# Read the .env file and set constants
+File.foreach('.env') do |line|
+  key, value =  line.strip.split('=')
+  eval "#{key}='#{value}'"
+end
+
+# Login to the Scene
+mechanize = Mechanize.new
+login_page = mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}")
+login_form = login_page.form
+login_form.field_with(id: 'password').value = PASSWORD
+page = mechanize.submit(login_form)
+
+# GET the list of the picture ids
+picture_id_list = []
+body = nil
+offset = 0
+bulk_size = 1000
+while offset < 2000
+  body = JSON.parse(mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}/ajax/photos?f=#{offset}&n=#{bulk_size}&s=defaultSort").body)
+  body.each{|e| picture_id_list << e['id']}
+  offset += bulk_size
+  puts offset
+  offset = 1000000000000 if body.size == 0
+  sleep 1
+end
+
+# Download the pictures
+Dir.chdir 'download' do
+  picture_id_list.each do |id|
+    mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}/photos/#{id}/img/p").save("#{id}.png")
+  end
+end
