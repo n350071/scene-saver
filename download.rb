0ef7e365b3f44431a8352d6594e7ad485b0403f3
n350071 <n350071@gmail.com>
2019-10-27 09:46:10 +0900

    :+1: Add the Dates to the file name and the attributes

--- download.rb
+++ download.rb
@@ -16,13 +16,13 @@ login_form.field_with(id: 'password').value = PASSWORD
 page = mechanize.submit(login_form)
 
 # GET the list of the picture ids
-picture_id_list = []
+picture_list = []
 body = nil
 offset = 0
 bulk_size = 1000
 while offset < 2000
   body = JSON.parse(mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}/ajax/photos?f=#{offset}&n=#{bulk_size}&s=defaultSort").body)
-  body.each{|e| picture_id_list << e['id']}
+  body.each{|e| picture_list << {id: e['id'], name: e['date_taken']}}
   offset += bulk_size
   puts offset
   offset = 1000000000000 if body.size == 0
@@ -31,11 +31,13 @@ end
 
 # Download the pictures
 Dir.chdir 'download' do
-  picture_id_list.each.with_index(1) do |id, idx|
-    mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}/photos/#{id}/img/p").save("#{id}.png")
+  picture_list.each.with_index(1) do |pic, idx|
+    mechanize.get("https://a.scn.jp/priv/#{ALBUM_URL}/photos/#{pic[:id]}/img/p").save("#{pic[:name]}.png")
+    time = Time.new(pic[:name][0,4],pic[:name][4,2],pic[:name][6,2],pic[:name][8,2],pic[:name][10,2],pic[:name][12,2])
+    File.utime(time, time, "#{pic[:name]}.png")
     sleep 0.1
     system "clear"
-    puts  "progress: #{idx} / #{picture_id_list.size}"
+    puts  "progress: #{idx} / #{picture_list.size}"
   end
 end
 
